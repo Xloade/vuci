b60a874b510305078f53118f0eee7c89caf09d61
dainius <dainius.cepulis@teltonika.lt>
2022-03-02 15:01:18 +0200

    refactored host to component

---
+++ vuci-ui-core/vue/src/views/Host.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="host">
+        <div class="logo">
+            <slot name="logo">
+                    <RouterSvg v-if="host.vendor=='Teltonika'"/>
+                    <DesktopSvg v-else/>
+            </slot>
+        </div>
+        <div class="info">
+            <slot name="info">
+                    <div v-if="host.knownHost">Name: {{host.discription}}</div>
+                    <div>Ip: {{host.ip}}</div>
+                    <div>Mac: {{host.mac}}</div>
+                    <div class="portStatus" v-if="host.ports">
+                        Ports:
+                        <div class="portStatusDot" :style="{background: anyOpenPorts(host.ports)?'green':'red'}"></div>
+                    </div>
+            </slot>
+        </div>
+        <div class="info aditional-info">
+            <slot name="info-aditional">
+                    <div>Vendor: {{host.vendor}}</div>
+                    <a-table rowKey="port" class="ports" v-if="host.ports" :data-source="host.ports" :columns="pingTableCols" >
+                        <span slot="status" slot-scope="status">
+                        <a-tag
+                            :color="status === 'open' ? 'green' : status === 'closed' ? 'red' : 'orange'"
+                        >
+                            {{ status.toUpperCase() }}
+                        </a-tag>
+                        </span>
+                    </a-table>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import RouterSvg from './RouterSvg'
+import DesktopSvg from './DesktopSvg'
+export default {
+  props: ['host'],
+  data () {
+    return {
+      pingTableCols: [
+        {
+          title: 'Port',
+          dataIndex: 'port'
+        },
+        {
+          title: 'Used for',
+          dataIndex: 'service'
+        },
+        {
+          title: 'Status',
+          dataIndex: 'status',
+          scopedSlots: { customRender: 'status' }
+        }
+      ]
+    }
+  },
+  components: {
+    DesktopSvg,
+    RouterSvg
+  },
+  methods: {
+    anyOpenPorts (ports) {
+      let res = false
+      ports.forEach(element => {
+        if (element.status === 'open') res = true
+      })
+      return res
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .logo{
+    width: 100px;
+    height: 100px;
+    z-index: 9;
+    display: flex;
+    background: #9b9696;
+  }
+  .logo > *{
+    width: 100px;
+    height: 100px;
+  }
+  .knownHost .logo svg{
+    fill: green;
+  }
+  .host:hover{
+    width: 400px;
+    transform: translate(-200px, -75px);
+    z-index: 10;
+    background: rgba(200,200,200);
+    logo-shadow: 3px 7px 3px 7px rgba(100,100,100,0.5);
+  }
+  .host:hover .logo{
+    background: rgba(200,200,200);
+  }
+  .info{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+  }
+  .info.aditional-info{
+    display:none;
+  }
+  .host:hover .info.aditional-info{
+    display:flex;
+  }
+  .ports{
+    background: white;
+    padding: 3px;
+    border-radius: 3px;
+    margin: 5px 0px
+  }
+  .portStatusDot{
+    width: 12px;
+    height: 12px;
+    border-radius: 6px;
+    display: inline-block;
+  }
+  .host{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+    border-radius: 10px;
+    padding: 25px;
+    z-index:2;
+    transform: translate(-110px, -75px);
+    cursor: default;
+    width: 220px
+  }
+</style>
--- vuci-ui-core/vue/src/views/vuci-app-network-scan.vue
+++ vuci-ui-core/vue/src/views/vuci-app-network-scan.vue
@@ -9,31 +9,7 @@
           </div>
           <div class="subnet" v-for="(subnet, subnetIndex) in subnets" :key="subnet.discription">
             <div @mousedown.stop :class="['host',{knownHost:host.knownHost}]" v-for="(host, index) in subnet.hosts" :key="host.ip" :style="getPositionStyle(index, getHostNumFromSubNet(subnet), getOffsetY(subnetIndex))">
-              <div class="box">
-                <RouterSvg v-if="host.vendor=='Teltonika'"/>
-                <DesktopSvg v-else/>
-              </div>
-              <div class="info">
-                <div v-if="host.knownHost">Name: {{host.discription}}</div>
-                <div>Ip: {{host.ip}}</div>
-                <div>Mac: {{host.mac}}</div>
-                <div class="portStatus" v-if="host.ports">
-                  Ports:
-                  <div class="portStatusDot" :style="{background: anyOpenPorts(host.ports)?'green':'red'}"></div>
-                </div>
-              </div>
-              <div class="info aditional-info">
-                <div>Vendor: {{host.vendor}}</div>
-                <a-table rowKey="port" class="ports" v-if="host.ports" :data-source="host.ports" :columns="pingTableCols" >
-                  <span slot="status" slot-scope="status">
-                    <a-tag
-                      :color="status === 'open' ? 'green' : status === 'closed' ? 'red' : 'orange'"
-                    >
-                      {{ status.toUpperCase() }}
-                    </a-tag>
-                  </span>
-                </a-table>
-              </div>
+              <Host :host="host"/>
             </div>
             <svg class="lines" :style="[{width:`${neededWidth}px`},{height:`${neededHeight}px`}]">
               <line v-for="(host, index) in subnet.hosts" :key="host.ip + '1'"
@@ -42,19 +18,22 @@
                 :y2="getPosition(index, getHostNumFromSubNet(subnet), getOffsetY(subnetIndex)).top" stroke="black" />
             </svg>
           </div>
-          <div class="host myhost" @mousedown.stop :style="[{top:`${neededHeight/2}px`},{left:`${neededWidth/2}px`}]">
-            <div class="box">
+
+          <Host class="host myhost" @mousedown.stop :style="[{top:`${neededHeight/2}px`},{left:`${neededWidth/2}px`}]">
+            <template v-slot:logo>
                 <RouterSvg/>
+            </template>
+            <template v-slot:info>
+              <div v-for="subnet in subnets" :key="subnet.discription+'my'">
+                <div>{{subnet.discription}}</div>
+                <div>Mac: {{subnet.myHost.mac}}</div>
+                <div>Ip: {{subnet.myHost.ip}}</div>
               </div>
-            <div class="info" v-for="subnet in subnets" :key="subnet.discription+'my'">
-              <div>{{subnet.discription}}</div>
-              <div>Mac: {{subnet.myHost.mac}}</div>
-              <div>Ip: {{subnet.myHost.ip}}</div>
-            </div>
-            <div class="info aditional-info">
+            </template>
+            <template v-slot:info-aditional>
               <div>Vendor: {{subnets[0].myHost.vendor}}</div>
-            </div>
-          </div>
+            </template>
+          </Host>
         </div>
       </ZoomDragViewport>
     </div>
@@ -67,9 +46,9 @@
 <script>
 import ZoomDragViewport from './ZoomDragViewport'
 import RouterSvg from './RouterSvg'
-import DesktopSvg from './DesktopSvg'
 import Progress from './Progress'
 import ScanOptions from './ScanOptions'
+import Host from './Host'
 export default {
   data () {
     return {
@@ -78,22 +57,7 @@ export default {
       neededHeight: 2000,
       progress: [],
       inProgress: false,
-      pingTimeout: null,
-      pingTableCols: [
-        {
-          title: 'Port',
-          dataIndex: 'port'
-        },
-        {
-          title: 'Used for',
-          dataIndex: 'service'
-        },
-        {
-          title: 'Status',
-          dataIndex: 'status',
-          scopedSlots: { customRender: 'status' }
-        }
-      ]
+      pingTimeout: null
     }
   },
   methods: {
@@ -179,13 +143,6 @@ export default {
     },
     widthFromItemCount (count) {
       return count * 200 + 200
-    },
-    anyOpenPorts (ports) {
-      let res = false
-      ports.forEach(element => {
-        if (element.status === 'open') res = true
-      })
-      return res
     }
   },
   computed: {
@@ -200,68 +157,19 @@ export default {
   components: {
     ZoomDragViewport,
     RouterSvg,
-    DesktopSvg,
     Progress,
-    ScanOptions
+    ScanOptions,
+    Host
   }
 }
 </script>
 
 <style scoped>
-  .box{
-    width: 100px;
-    height: 100px;
-    z-index: 9;
-    display: flex;
-    background: #9b9696;
-  }
-  .box > *{
-    width: 100px;
-    height: 100px;
-  }
-  .host{
-    position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-flow: column;
-    border-radius: 10px;
-    padding: 25px;
-    z-index:2;
-    transform: translate(-110px, -75px);
-    cursor: default;
-    width: 220px
-  }
-  .knownHost .box svg{
-    fill: green;
-  }
-  .host:hover{
-    width: 400px;
-    transform: translate(-200px, -75px);
-    z-index: 10;
-    background: rgba(200,200,200);
-    box-shadow: 3px 7px 3px 7px rgba(100,100,100,0.5);
-  }
-  .host:hover .box{
-    background: rgba(200,200,200);
-  }
   .lines{
     position: absolute;
     top: 0px;
     left: 0px;
     z-index: -1;
-  }
-  .info{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-flow: column;
-  }
-  .info.aditional-info{
-    display:none;
-  }
-  .host:hover .info.aditional-info{
-    display:flex;
   }
   .subnet-discription{
     font-size: 50px;
@@ -285,16 +193,7 @@ export default {
   .btn{
     margin: 5px
   }
-  .ports{
-    background: white;
-    padding: 3px;
-    border-radius: 3px;
-    margin: 5px 0px
-  }
-  .portStatusDot{
-    width: 12px;
-    height: 12px;
-    border-radius: 6px;
-    display: inline-block;
+  .host{
+    position: absolute;
   }
 </style>
