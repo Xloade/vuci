9c5abea828953e2148e1853a164edae66e4d3adb
dainius <dainius.cepulis@teltonika.lt>
2022-03-07 09:26:36 +0200

    overlay when empty to enable scroll

---
+++ vuci-ui-core/vue/src/views/Overlay.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="overlay">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+  .overlay{
+    position: absolute;
+    top:0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(175,175,175,0.80);
+    z-index: 100;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column
+  }
+</style>
--- vuci-ui-core/vue/src/views/Progress.vue
+++ vuci-ui-core/vue/src/views/Progress.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="overlay">
-        <h1>{{$t("In progress")}}</h1>
-        <div class="subnet" v-for="subnet in progress" :key="subnet.discription">
-            <h1>{{subnet.discription}}:</h1>
-            <div class="progress-border">
-                <a-progress :percent="subnet.progress.done? 100:parseFloat(subnet.progress.donePct)" :status="subnet.progress.done ? 'success': 'active'" />
-            </div>
-            <p v-if="!subnet.progress.done">{{$t("estimated/remaining", {elapsed:subnet.progress.timeElapsed, remaining:subnet.progress.remainingTime})}}</p>
-        </div>
+  <div>
+    <h1>{{$t("In progress")}}</h1>
+    <div class="subnet" v-for="subnet in progress" :key="subnet.discription">
+      <h1>{{subnet.discription}}:</h1>
+      <div class="progress-border">
+        <a-progress :percent="subnet.progress.done? 100:parseFloat(subnet.progress.donePct)" :status="subnet.progress.done ? 'success': 'active'" />
+      </div>
+      <p v-if="!subnet.progress.done">{{$t("estimated/remaining", {elapsed:subnet.progress.timeElapsed, remaining:subnet.progress.remainingTime})}}</p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -18,21 +18,7 @@ export default {
 </script>
 
 <style scoped>
-    .overlay{
-        position: absolute;
-        top:0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: rgba(175,175,175,0.80);
-        z-index: 100;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column
-    }
-    .subnet{
-        min-width: 300px;
-    }
+  .subnet{
+    min-width: 300px;
+  }
 </style>
