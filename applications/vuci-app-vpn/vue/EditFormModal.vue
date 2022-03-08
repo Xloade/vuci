382a8421eed498dadce6e456a51d5b3b3202d36e
dainius <dainius.cepulis@teltonika.lt>
2022-03-08 10:24:57 +0200

    added modal

---
+++ applications/vuci-app-vpn/vue/EditFormModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <a-modal
+      title="Edit VPN"
+      :visible="visible"
+      okText="Create"
+      :cancelButtonProps="{props: {type:'danger'}}"
+      @ok="handleCreate"
+      @cancel="handleCancel"
+    >
+      <a-form-model ref="form" :rules="rules" :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-form-model-item :label="$t('form.isActive')">
+          <a-switch v-model="form.isActive"/>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  props: ['type'],
+  data () {
+    return {
+      visible: false,
+      labelCol: { span: 6 },
+      wrapperCol: { span: 12 },
+      rules: {
+      },
+      form: {
+        isActive: true
+      }
+    }
+  },
+  methods: {
+    showModal () {
+      this.visible = true
+    },
+    handleCreate (e) {
+
+    },
+    handleCancel (e) {
+      this.visible = false
+    }
+  }
+}
+</script>
--- applications/vuci-app-vpn/vue/app.vue
+++ applications/vuci-app-vpn/vue/app.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    Vpn
+    <add-edit-form-modal ref="formModal"/>
+    <a-button type="primary" @click="$refs.formModal.showModal()">
+      Create
+    </a-button>
   </div>
 </template>
 
 <script>
+import AddEditFormModal from './EditFormModal.vue'
 export default {
-
+  components: {
+    AddEditFormModal
+  }
 }
 </script>
 
