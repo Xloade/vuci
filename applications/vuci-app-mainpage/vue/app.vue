e51843401afbe07c2cf5687436499798007fccfe
dainius <dainius.cepulis@teltonika.lt>
2022-03-31 11:56:22 +0300

    updated settings pannel

--- applications/vuci-app-mainpage/vue/app.vue
+++ applications/vuci-app-mainpage/vue/app.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <a-modal v-model="visible" title="Show/Hide Cards" @ok="handleOk" @cancel="handleCancel" centered>
-      <template slot="footer">
-        <a-button key="submit" @click="handleOk">
-          Done
-        </a-button>
-      </template>
-      <a-form-model :label-col="{ span: 6 }" :wrapper-col="{ span: 14 }">
-        <div v-for="card in cards" :key="card.name" >
-          <a-form-model-item :label="$t(card.name)" v-if="card.name !== 'wan6Card'">
-            <a-switch  v-model="card.info.show" @change="syncFiltered"/>
-          </a-form-model-item>
-        </div>
-      </a-form-model>
-    </a-modal>
-    <a-button class="openModalBtn" type="primary" @click="showModal">
-      Show/Hide Cards
+    <div class="toggles" :style="{width: `${visible ? 300: 0}px`}">
+      <div class="toggle-inner">
+        <h3>Show/hide information cards</h3>
+        <a-form-model class="form" :label-col="{ span: 12 }" :wrapper-col="{ span: 12 }">
+          <div v-for="card in cards" :key="card.name" >
+            <a-form-model-item :label="$t(card.name)" v-if="card.name !== 'wan6Card'">
+              <a-switch  v-model="card.info.show" @change="syncFiltered"/>
+            </a-form-model-item>
+          </div>
+        </a-form-model>
+      </div>
+    </div>
+    <a-button class="openModalBtn" type="primary" @click="toggleVisibility" :style="{right: `${visible ? 300: 0}px`}">
+      <settings-svg class="settings-svg"/>
     </a-button>
     <a-row :gutter="[12,12]" type="flex">
       <draggable class="drag" @change="saveOrder" :list="filteredCards" @start="drag=true" @end="drag=false" :animation="200" ghost-class="ghost-card">
@@ -36,6 +34,7 @@ import WanCard from './WANCard.vue'
 import Wan6Card from './WAN6Card.vue'
 import AccessCard from './AccessCard.vue'
 import draggable from 'vuedraggable'
+import SettingsSvg from './settingsSvg.vue'
 export default {
   data () {
     return {
@@ -80,15 +79,9 @@ export default {
           this.syncFiltered()
         })
     },
-    showModal () {
-      this.visible = true
-    },
-    handleOk (e) {
-      this.saveShow()
-      this.visible = false
-    },
-    handleCancel (e) {
-      this.saveShow()
+    toggleVisibility () {
+      if (this.visible) this.saveShow()
+      this.visible = !this.visible
     }
   },
   created () {
@@ -102,7 +95,8 @@ export default {
     WanCard,
     Wan6Card,
     AccessCard,
-    draggable
+    draggable,
+    SettingsSvg
   }
 }
 </script>
@@ -117,9 +111,39 @@ export default {
   border: 1px solid #4299e1;
 }
 .openModalBtn{
-  margin:12px
+  height: 60px;
+  width: 35px;
+  border-radius: 5px 0px 0px 5px;
+  padding: 5px;
+  position: absolute;
+  top: 120px;
+  z-index: 10000;
 }
 .drag{
   width: 100%;
+}
+.settings-svg{
+  fill: white
+}
+.toggles{
+  position: absolute;
+  top: 100px;
+  right: 0px;
+  z-index: 10000;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  border: 1px solid #c2c2c2;
+  background: white;
+  border-radius: 5px 0px 0px 5px;
+  overflow: hidden;
+}
+.toggle-inner{
+  padding: 12px;
+  width: 300px !important;
+}
+.form{
+  width: 100%;
+}
+.toggle-inner h3{
+  margin: 20px;
 }
 </style>
